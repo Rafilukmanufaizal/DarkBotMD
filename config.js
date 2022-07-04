b1af0a71e9a6d1ba5d424d6447e1fa65180e0714
Rafi Lukmanu Faizal <104605530+EzinterOfc@users.noreply.github.com>
2022-07-04 11:00:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281327441039']
-global.premium = ['6281327441039']
+global.owner = ['6285600380823']
+global.premium = ['6285600380823']
 global.packname = 'Hisoka Morrow'
-global.author = 'Mr_Dark'
+global.author = 'Faisal'
 global.sessionName = 'session'
 global.prefa = ['/','!','.','#',',','_']
 global.sp = '⭔'
